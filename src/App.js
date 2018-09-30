c8cfa2b1d368aee969805f53e40089075928d9b6
Vivek <vivekbhardwaj@nevada.unr.edu>
2018-09-30 10:30:26 -0700

    Added Navbar Unsuccesfully

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Look at me I'm Noaaahh. I just want to make vivek kill himselffff.
-        </p>
-      </div>
-    );
+    return <Navbar />;
   }
 }
 
