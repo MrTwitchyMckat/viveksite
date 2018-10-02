b6f56825aa868af8b2b82a6b8cc3a8b37cd178fa
Vivek Bhardwaj <minimememachine@Viveks-MacBook-Pro.local>
2018-10-02 09:37:49 -0700

    Added Navbar components that dont use jquery, also added logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,50 +1,21 @@
 import React, { Component } from "react";
+import logo from '../logo.svg';
 
 class Navbar extends Component {
   state = {};
   render() {
     return (
-      <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
-        <div class="container">
-          <a class="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon" />
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">
-                  Home <span class="sr-only">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#">
-                  Disabled
-                </a>
+      <nav className="navbar navbar-sticky navbar-dark bg-dark">
+        <a className="navbar-brand" href="/">
+          <img src={logo} width="30" height="30" alt=""/>
+         </a>
+         <div className="" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
               </li>
             </ul>
           </div>
-        </div>
       </nav>
     );
   }
