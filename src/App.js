0cd94341498cb5a2f18f3f84354ff3eeb62c7fcc
Vivek Bhardwaj <minimememachine@Viveks-MacBook-Pro.local>
2018-10-02 10:02:42 -0700

    added font awesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faFolder, faHome} from '@fortawesome/pro-regular-svg-icons';
 import Navbar from "./components/navbar";
+
+library.add(faFolder, faHome);
+
+
 
 class App extends Component {
   render() {
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import logo from '../logo.svg';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
 
 class Navbar extends Component {
   state = {};
   render() {
     return (
       <nav className="navbar navbar-sticky navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">
-          <img src={logo} width="30" height="30" alt=""/>
-         </a>
-         <div className="" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-              </li>
-            </ul>
-          </div>
+       <div className="container">
+          <a className="navbar-brand" href="/">
+            <img src={logo} width="30" height="30" alt=""/>
+          </a>
+          <div className="" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                <a className="nav-link" href="/"><FontAwesomeIcon icon={["far", "home"]}/> Home <span class="sr-only">(current)</span></a>
+                </li>
+              </ul>
+            </div>
+        </div>
       </nav>
     );
   }
