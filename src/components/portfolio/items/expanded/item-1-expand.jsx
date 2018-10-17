4d4ff383f7103db5ce038eef7b308ddd4a47e30a
Vivek Bhardwaj <minimememachine@Viveks-MacBook-Pro.local>
2018-10-16 17:07:32 -0700

    beginning to add in expanded-items

--- src/components/portfolio/items/expanded/item-1-expand.jsx
+++ src/components/portfolio/items/expanded/item-1-expand.jsx
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import "./expanded.css";
 import img1 from "../../../../img/smallImg/effortless-small.jpg";
 
+
 class ItemOneExpand extends Component {
   state = {};
   render() {
     return(
       <div className="container-fluid full-screen">
-        <div className="row">
-          <img src={img1} alt="effortless"/>
-        </div>
+      <div className="col-lg-12">
+        <h1 class="white-text text-center">EFFORTLESS IT</h1>
+        <img src={img1} class="text-center" alt="effortless"/>
+      </div>
       </div>
     );
   }
