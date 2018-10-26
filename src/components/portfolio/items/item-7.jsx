e7eef9de6baf137bef47c9a0e0705971838bb1de
Vivek Bhardwaj <minimememachine@Viveks-MacBook-Pro.local>
2018-10-26 15:16:31 -0700

    Added last two expanded items

--- src/components/portfolio/items/item-7.jsx
+++ src/components/portfolio/items/item-7.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
 import "./items.css";
+import ExpandSeven from "./expanded/item-7-expand";
 
 class ItemSeven extends Component {
-  state = {};
+  state = {
+    expanded: false
+  };
+
+  expandClickHandler = () => {
+    this.setState(prevState => {
+      return({expanded: !prevState.expanded});
+    });
+  };
+
+  closeClickHandler = () =>{
+    this.setState({expanded: false});
+  }
   render() {
+    let expandedSeven;
+
+    if(this.state.expanded) {
+      expandedSeven = <ExpandSeven closeHandler={this.closeClickHandler}/>;
+    }
+
     return (
-      <div className="itemBox itemBox7 col-md-3">
+      <div className="itemBox itemBox7 col-md-3" onClick={this.expandClickHandler}>
         <h1 className="">Sorry Crew</h1>
+        {expandedSeven}
       </div>
     );
   }
