e2a83ce06c6ede3676f92e9f397443bd8af427c7
Vivek Bhardwaj <minimememachine@Viveks-MacBook-Pro.local>
2018-11-06 10:13:30 -0800

    More About Changes

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./about.css";
 
 class About extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                <div className="container-fluid about-sec">
-                    <div className="row">
-                    <div className="col-12"><h1 className="text-center about-header">ABOUT ME</h1></div>
-                        
-                            <div className="col-3">
-                                <img src="#" alt="yeet"/>
-                            </div>
-                            <div className="col-9">
-                                <h1>yeet</h1>
-                            </div>
-                    </div>
-                </div>
-            </div>  
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <div className="container-fluid about-sec">
+          <div className="row">
+            <div className="col-12">
+              <h1 className="text-center about-header">ABOUT ME</h1>
+            </div>
+
+            <div className="col-3">
+              <img src="#" alt="yeet" />
+            </div>
+            <div className="col-9">
+              <h1>yeet</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default About;
