09671991964b62725b8aa76d548468fd33ad03fd
Vivek <vivekbhardwaj@nevada.unr.edu>
2018-12-13 01:02:15 -0800

    Finished adding all portfolio items

--- src/components/portfolio/items/expanded/item-6-expand.jsx
+++ src/components/portfolio/items/expanded/item-6-expand.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import "./expanded.css";
-import EffortlessMockup from "../../../../img/largeImg/effotless-mockup.png";
-import OCGlogo from "../../../../img/largeImg/OCG_LOGO.png";
-import EffortlessLogo from "../../../../img/largeImg/Effortless_IT_Consulting_Logo_RGB.png";
-import EffortlessScroll from "../../../../img/largeImg/effortless-home-scroll.gif";
+import NSHE from "../../../../img/largeImg/nshe.png";
 
 import CloseButtonX from "./closeButtonX";
 
@@ -20,59 +17,19 @@ class ItemSixExpanded extends Component {
             <CloseButtonX onClick={this.props.closeHandler} />
           </div>
           <h1 className="text-center red-text pad-t2">
-            EFFORTLESS <strong>IT</strong>
+            NEVADA SYSTEM OF <strong>HIGHER EDUCATION</strong>
           </h1>
-          <h3 className="ocgHeader">
-            <em>Designed at</em>
-          </h3>
-          <a
-            href="http://ocgcreative.com"
-            rel="noopener noreffer"
-            target="blank"
-          >
-            <img
-              src={OCGlogo}
-              className="img-fluid mx-auto d-block"
-              alt="OCG Creative"
-            />
-          </a>
           <img
-            src={EffortlessMockup}
-            className="img-fluid mx-auto d-block"
-            alt="effortless"
+            src={NSHE}
+            className="img-fluid mx-auto d-block pad-t3 pad-b3"
+            alt="nevada system of higher education"
           />
-          <img
-            src={EffortlessLogo}
-            className="img-fluid mx-auto d-block"
-            alt="effortless webdesign"
-          />
-          <img
-            src={EffortlessScroll}
-            className="img-fluid mx-auto d-block"
-            alt="effortless webdesign"
-          />
-          <a
-            href="http://ocgcreative.com"
-            rel="noopener noreffer"
-            target="blank"
-          >
-            <h1 className="text-center bottom-link">WWW.EFFORTLESSIT.XYZ</h1>
-          </a>
           <div className="row">
             <div className="col-md-3">
               <br />
             </div>
             <div className="col-md-5">
-              <p className="grey-text">
-                Designed using WordPress, Effortless IT was a website that came
-                to me naturally. The gradient color theme as well as the font
-                were pulled from the logo and business cards. This made
-                development a breeze since there was really no guess work
-                involved design-wise. From there graphics were made on a page by
-                page basis and the site came together nicely. Just to add some
-                flare the we used Typed JS on a few pages to add the{" "}
-                <em>"technology vibe."</em>
-              </p>
+              <p className="grey-text" />
             </div>
             <div className="col-md-3">
               <br />
