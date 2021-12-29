d90ecd75c7485dc43dc514d1746957954289af11
Vivek <vivekbhardwajnv@gmail.com>
2021-12-29 00:52:02 -0800

    add sass dependency. add loading animation screen

---
+++ src/components/Loading.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './loading.scss';
+
+class Loading extends React.Component {
+  render() { 
+    return (
+      <div className="loading">
+        <div className="loading__container">
+          <p className="glitch3">loading...</p>
+          <p className="glitch2">loading..</p>
+          <p className="glitch1">loading...</p>
+        </div>
+      </div>
+    );
+  }
+}
+ 
+export default Loading;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import Loading from './components/Loading';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Loading />
   </React.StrictMode>,
   document.getElementById('root')
 )
